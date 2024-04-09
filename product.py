08671be88407dacfa74509002fc49f331de49442
ayebaishmo <ishmo255super@gmail.com>
2024-04-09 13:06:23 +0300

    Started on the product module

---
+++ product.py
@@ -0,0 +1,9 @@
+class Product:
+    
+    def __init__(self, name, identifier, price=10, weight=20, flammability=0.5):
+        self.name = name
+        self.identifier = identifier
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+    
