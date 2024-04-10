615bfc696e3aabc2d8f7f82a62da831ce5211834
ayebaishmo <ishmo255super@gmail.com>
2024-04-10 11:42:24 +0300

    Fixed lint errors for acme.py

--- acme.py
+++ acme.py
@@ -1,6 +1,16 @@
+"""
+This is python module that creates products
+and performs some methods on them
+"""
 import random
 
+
 class Product:
+    """
+    Above is a class called Product
+    This class create a product 
+    with several aspects
+    """
     def __init__(self, name, price=10, weight=20, flammability=0.5):
         self.name = name
         self.price = price
@@ -9,24 +19,34 @@ class Product:
         self.identifier = random.randint(1000000, 9999999)
 
     def stealability(self):
+        """
+        This function calculates the 
+        ratio of price to weight
+        """
         ratio = self.price / self.weight
         if ratio < 0.5:
             return "Not so stealable..."
-        elif ratio >= 0.5 or ratio < 1.0:
+        if ratio >= 0.5 or ratio < 1.0:
             return "Kinda stealable."
-        else:
-            return "Very stealable!"
+        return "Very stealable!"
 
     def explode(self):
+        """
+        This function calculates
+        the product
+        """
         product = self.flammability * self.weight
         if product < 10:
             return "...fizzle."
-        elif product >= 10 and product < 50:
+        if 10 <= product < 50:
             return "...boom!"
-        else:
-            return "...BABOOM!!"
+        return "...BABOOM!!"
 
 class BoxingGlove(Product):
+    """
+    This is child class that 
+    inherits the product class
+    """
     def __init__(self, name, price=10, weight=10, flammability=0.5):
         super().__init__(name, price, weight, flammability)
 
@@ -34,9 +54,12 @@ class BoxingGlove(Product):
         return "...it's a glove."
 
     def punch(self):
+        """
+        This function checks
+        the weight and returns a statement
+        """
         if self.weight <= 5:
             return "That tickles."
-        elif self.weight > 5 and self.weight < 15:
+        if self.weight > 5 and self.weight < 15:
             return "Hey that hurt!"
-        else:
-            return "OUCH!"
+        return "OUCH!"
