51d02292849f8f803fdc69b97db63e2974e7beb5
ayebaishmo <ishmo255super@gmail.com>
2024-04-10 13:40:19 +0300

    Fixed lint errors in acme_test

--- acme_test.py
+++ acme_test.py
@@ -7,10 +7,12 @@ acme_report to ensure they are working
 from acme import Product
 from acme_report import generate_products
 
+
 def test_default_product_price():
     '''Test default product price being 10.'''
     prod = Product('Test Product')
     assert prod.price == 10
+
 
 def test_default_product_attributes():
     '''Test default product attributes.'''
@@ -18,11 +20,13 @@ def test_default_product_attributes():
     assert prod.weight == 20
     assert prod.flammability == 0.5
 
+
 def test_product_methods():
     '''Test stealability() and explode() methods.'''
     prod = Product('Test Product', price=5, weight=15, flammability=2.0)
     assert prod.stealability() == "Not so stealable..."
     assert prod.explode() == "...boom!"
+
 
 def test_generate_products():
     '''Test generate_products() function returns a list with 30 items.'''
